6e5c6862dbbdd799d404aa2db2da515948c35fc1
Ammar Yasser Tolba <ammartolba89@192.168.1.12>
2026-03-23 17:58:35 +0200

    update

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,36 +1,50 @@
-window.swiper = new Swiper(".mySwiper", {
-  loop: true,
-  slidesPerView: 1,
-  autoplay: { delay: 4500, disableOnInteraction: false },
-  speed: 1200,
-  effect: "fade",
-  fadeEffect: { crossFade: true },
-});
+if (document.querySelector(".mySwiper")) {
+  window.swiper = new Swiper(".mySwiper", {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: { delay: 4500, disableOnInteraction: false },
+    speed: 1200,
+    effect: "fade",
+    fadeEffect: { crossFade: true },
+  });
+}
 
-window.swiperMobile = new Swiper(".mySwiper-mobile", {
-  loop: true,
-  slidesPerView: 1,
-  autoplay: { delay: 3000 },
+if (document.querySelector(".mySwiper-mobile")) {
+  window.swiperMobile = new Swiper(".mySwiper-mobile", {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: { delay: 3000, disableOnInteraction: false },
+    speed: 1500,
+    effect: "slide",
+  });
+}
 
-  speed: 1500,
-  effect: "slide",
-});
+  window.projectSwiper = new Swiper(".project-swiper", {
+    loop: true,
+    speed: 800,
+    spaceBetween: 24,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".project-swiper .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".project-swiper .swiper-button-next",
+      prevEl: ".project-swiper .swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+    },
+  });
 
-
-window.projectSwiper = new Swiper(".project-swiper", {
-  slidesPerView: 3,
-  speed: 1500,
-  spaceBetween: 30,
-  autoplay: { delay: 3000 },
-  loop: true,
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    320: { slidesPerView: 1 },
-    768: { slidesPerView: 2 },
-    1200: { slidesPerView: 2 },
-  },
-});
